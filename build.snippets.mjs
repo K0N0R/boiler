2fa4c369349170d6217139949ee337d5892626cb
k0n0r <maciej.drzewicki@insert.com.pl>
2025-08-27 18:01:37 +0200

    build snippet

--- build.snippets.mjs
+++ build.snippets.mjs
@@ -4,6 +4,10 @@ export async function copyFrontendFiles(outDir, htmlReplacement = '') {
     let html = await fs.readFile('frontend/index.html', 'utf8');
     html = html.replace('<!-- inject:reload-script -->', htmlReplacement);
     await fs.writeFile(`${outDir}/public/index.html`, html);
+    await copyAssets(outDir);
+}
+
+export async function copyAssets(outDir) {
     await fs.copyFile('frontend/preload-config.json', `${outDir}/public/preload-config.json`);
     await fs.cp('frontend/assets', `${outDir}/public/assets`, { recursive: true });
 }
